f3621a0f20c4a0a6227a272a1b7067d1734391ce
Aliaksei Hurynovich <aliaksei_hurynovich@epam.com>
2018-03-01 13:58:40 +0300

    Fixed issue with keys on HomePage

--- src/client/js/components/HomePage/index.jsx
+++ src/client/js/components/HomePage/index.jsx
@@ -3,25 +3,25 @@ import { Route, Redirect, Link } from 'react-router-dom';
 import './styles.scss';
 
 const HomePage = ({isLoggedIn, onLogout, user}) => (
-	<div className='homePage'>
+	<div className="homePage">
 		{!isLoggedIn ? 
-			[
+			<div className="homePageInner">
 				<span className="infoText">
 					Hello! Welcome to BlogsApp!
-				</span>,
+				</span>
 				<span className="infoText">
 					Please <Link className="lnk loginLnk" to="/login">Log in</Link> or <Link className="lnk registerLnk" to="/register">Register</Link> to proceed.
 				</span>
-			] :
-			[
+			</div> :
+			<div className="homePageInner">
 				<span className="infoText">
 					Hello, <span className="userName">{user.username}</span>! Welcome to BlogApps!
-				</span>,
+				</span>
 				<span>
 					Feel free to check <Link className="lnk blogsListLnk" to="/blogs">Blogs list</Link>.
-				</span>,
+				</span>
 				<a href="#" className="lnk" onClick={() => onLogout()}>Log out</a>
-			]
+			</div>
 		}
 	</div>
 )
