c03235de8760981e45a97141c0995c1c7e4661a5
Aliaksei Hurynovich <aliaksei_hurynovich@epam.com>
2018-03-02 18:21:42 +0300

    Fixed issue with component testing

--- src/client/js/components/HomePage/__tests__/HomePage.spec.js
+++ src/client/js/components/HomePage/__tests__/HomePage.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomePage from '../index.jsx';
+import StaticRouter from 'react-router';
 
 describe('HomePage component', () => {
 	const user = {
@@ -9,15 +10,16 @@ describe('HomePage component', () => {
 	const onLogout = jest.fn();
 
 	it('renders "Please Log In or Register message" if user is not logged in', () => {
-		const wrapper = shallow(<HomePage isLoggedIn={false} onLogout={onLogout} user={null}/>)
+		const wrapper = shallow(<HomePage isLoggedIn={false} onLogout={onLogout} user={null}/>);
 		expect(wrapper).toMatchSnapshot();
 
-		wrapper.find('.logoutLink').simulate('click');
-		expect(onLogout).toBeenCalled();
 	});
 	
 	it('renders "Welcome to BlogsApp" message if user is logged in', () => {
 		const wrapper = shallow(<HomePage isLoggedIn={true} onLogout={onLogout} user={user}/>);
 		expect(wrapper).toMatchSnapshot();
+
+		wrapper.find('.logoutLink').simulate('click');
+		expect(onLogout).toBeCalled();
 	})
 })
